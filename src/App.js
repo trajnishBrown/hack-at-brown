d564ef2d204d4a85eb7578ed9d46153cfdc8e775
trajnishBrown <tarun_rajnish@brown.edu>
2022-01-29 12:39:51 -0500

    idea comments

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ import "@fontsource/roboto/700.css";
 import Button from "@mui/material/Button";
 
 /* Plan (Feel free to add on):
-Have a cool (greenish themed) landing page to be coded here.
+Have a cool (greenish themed) landing page to be coded here in App.js
+Maybe some cool/funny animations using svg or something I dont know. Doing
+some thing or telling a joke or giving instructions.
 Button at the center of the page that will go into the main functionality.
+(Reference is Hack@Brown website : https://2022.hackatbrown.org/)
 Maybe a data science dashboard from some dataset here: https://www.epa.gov/ghgreporting/data-sets
 */
 
 /* To implement (Feel free to add on):
-1) UI for landing page
+1) UI for landing page 
 2) react-router to route
 3) deploy to firebase for points (Tarun will take care)
 4) use gcp compute for some task (maybe)
@@ -22,6 +25,7 @@ Maybe a data science dashboard from some dataset here: https://www.epa.gov/ghgre
 6) Data visualization using some library 
 7) Data parsing and cleaning
 8) Some 3D animations if easy to implement (maybe)
+9) Other pages for other functionalities
 */
 
 function App() {
