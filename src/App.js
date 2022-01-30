0f4689b3bfb26a01b1a294b87accfc9b403f7e60
trajnishBrown <tarun_rajnish@brown.edu>
2022-01-29 21:09:06 -0500

    homepage work

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import Box from "@mui/material/Box";
 
 /* Plan (Feel free to add on):
 Have a cool (greenish themed) landing page to be coded here in App.js
@@ -31,9 +36,40 @@ Maybe a data science dashboard from some dataset here: https://www.epa.gov/ghgre
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant="contained">Hello World</Button>
-      </header>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="100%" sx={{ height: "100vh", bgcolor: "#CCE0CF" }}>
+          <Typography
+            variant="h1"
+            component="div"
+            gutterBottom
+            sx={{ color: "#0F0F0F" }}
+          >
+            Hack @ Brown
+          </Typography>
+          <Typography variant="h2" gutterBottom component="div">
+            The Greenhouse Webapp
+          </Typography>
+          <Typography variant="h6" gutterBottom component="div">
+            Greenhouse gases trap heat and make the planet warmer. Human
+            activities are responsible for almost all of the increase in
+            greenhouse gases in the atmosphere over the last 150 years.1 The
+            largest source of greenhouse gas emissions from human activities in
+            the United States is from burning fossil fuels for electricity,
+            heat, and transportation.
+          </Typography>
+          <div className="button-div">
+            <Button
+              variant="outlined"
+              className="button"
+              size="large"
+              href="#outlined-buttons"
+            >
+              Explore More
+            </Button>
+          </div>
+        </Container>
+      </React.Fragment>
     </div>
   );
 }
